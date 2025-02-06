f476473b2527f8f1812c3feebc1dd394232f35dc
BohdanBychak <ipz232_bbm@student.ztu.edu.ua>
2025-02-06 12:04:53 +0200

    Створено простий консольний проєкт на C#

---
+++ gitC#Task/gitC#Task/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace gitC_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+
+            while (true)
+            {
+                Console.Write("Введіть перше число: ");
+                if (double.TryParse(Console.ReadLine(), out num1))
+                    break;
+                Console.WriteLine("Некоректне введення! Введіть число.");
+            }
+
+            while (true)
+            {
+                Console.Write("Введіть друге число: ");
+                if (double.TryParse(Console.ReadLine(), out num2))
+                    break;
+                Console.WriteLine("Некоректне введення! Введіть число.");
+            }
+
+            double sum = num1 + num2;
+            Console.WriteLine($"Сума: {sum}");
+
+        }
+    }
+}
